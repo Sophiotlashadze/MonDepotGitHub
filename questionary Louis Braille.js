14326aa19e8b8af6834c6a8a6bc2e99f986a1b87
Sofio <sofio23@hotmaill.fr>
2022-05-17 10:28:20 +0200

    initial commit

---
+++ questionary Louis Braille.js
@@ -0,0 +1,72 @@
+// function FirstQuestion()
+//  {
+// let _firstNumber=Math.floor(Math.random()*100);    
+// let _secondNumber=Math.floor(Math.random()*100);
+// let _result=_firstNumber+_secondNumber
+// let _userAnswer=parseInt(prompt("calcule :\n"+_firstNumber+"+"+_secondNumber));
+// if(_result==_userAnswer)
+// {
+//     return "bon reponse";
+// }
+// else
+// {
+//     return "mouvais reponse,bon reponse etait:"+_result;
+
+// }
+// }
+// alert(FirstQuestion())
+
+
+//  first question
+
+
+ function firstQuestion()
+{
+    let _firstName("comment s'appelle mr. braille?");
+    if(_firstName==g1"Louis")
+{
+    return "c'est correct";
+}
+else
+{
+    return "ce n'est pas correct, c'est correct Louis";
+}
+    }
+    alert(firstQuestion())
+
+
+// Second question7
+
+
+function secondQuestion()
+{
+    let _answer=prompt("qui est louis braille?"+"\n"+"tape 1 musicien français"+"\n"+"tape 2 inventeur"+"\n"+"tape 3 enseignant");
+if (_answer==1)
+{
+    return"il est musicien français et toutes les trois sont coorect";
+}
+else if(_answer==2)
+{
+return"inventeur et toutes les trois sont correct";
+}
+else
+{
+    return "enseignant et toutes les trois sont correct"; 
+}
+}
+// alert(secondQuestion())
+
+// Third question
+
+function thirdQuestion()
+{
+    let _birthDate=prompt("date de naissance?");
+    let _deathDate=prompt("date de mort");   
+    let _question=prompt("quel mois est braille louis né et mort?");
+    result="jenvier";
+    if
+    {
+    let _birthDate=("jenuari");
+    }
+    )
+}
